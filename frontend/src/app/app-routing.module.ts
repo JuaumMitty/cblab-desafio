742434871ed0f6ff4fbde70df316b7884293bc33
JuaumMitty <jjoaovictorpg@gmail.com>
2021-10-25 17:05:53 -0300

    Adicionando componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarEditarUsuariosComponent } from './components/criar-editar-usuarios/criar-editar-usuarios.component';
+import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listarUsuarios', component: ListarUsuariosComponent },
+  { path: 'cdUsuarios', component: CriarEditarUsuariosComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,20 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { EditarUsuariosComponent } from './components/editar-usuarios/editar-usuarios.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { CrudService } from './services/crud.service';
+import { CriarEditarUsuariosComponent } from './components/criar-editar-usuarios/criar-editar-usuarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    EditarUsuariosComponent,
     NavbarComponent,
-    ListarUsuariosComponent
+    ListarUsuariosComponent,
+    CriarEditarUsuariosComponent
   ],
   imports: [
     BrowserModule,
